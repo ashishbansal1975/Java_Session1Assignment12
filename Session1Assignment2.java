ec322853189f2587f64ed5f390658a14b824a706
ashishbansal1975 <ashish_bansal01@yahoo.com>
2016-09-04 18:28:07 +0530

    Hi

---
+++ Session1Assignment2.java
@@ -0,0 +1,19 @@
+public class Session1Assignment2
+{
+    public static void main(String [] args)
+    {
+            int a,b;
+            a = 20;
+            b = 10;                
+            
+            System.out.print("b = "); 
+            System.out.println(a-- - --a);            
+            System.out.print("c = ");
+            System.out.println(a--);
+            System.out.print("d = ");
+            System.out.println(a>>2);                        
+            System.out.print("e = ");
+            System.out.println(a&b);
+     }
+}
+
